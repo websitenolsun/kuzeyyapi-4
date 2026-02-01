a806f8b62b7267dc4f83b26c76e7f108c6ef0351
websitenolsun <wwebsitenolsun@gmail.com>
2026-02-01 20:42:33 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { componentTagger } from "lovable-tagger";
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+}));
